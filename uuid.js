d03ab661b82edb8beb9fa120b2c4f9f4925bb409
nani040 <31981066+nani040@users.noreply.github.com>
2017-12-21 11:07:45 +0530

    first
    First Commit

---
+++ uuid.js
@@ -0,0 +1,13 @@
+const uuidv4 = require('uuid/v4');
+const uuidv1 = require('uuid/v1');
+
+var id4 = uuidv4();
+var id1 = uuidv1();
+var reply = {
+	name: "nan",
+	id5 : uuidv4()
+}
+
+console.log("id1: "+id1);
+console.log("id4: "+id4);
+console.log("reply.id: "+reply.id5);
